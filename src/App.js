03f352220a5692fabee8df7a96989b4da4d3d527
dipsDeepti <65267352+dipsDeepti@users.noreply.github.com>
2020-10-20 00:04:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './screens/login/Login';
 import Home from './screens/home/Home';
 import Profile from './screens/Profile/Profile';
@@ -11,16 +11,16 @@ class App extends Component {
     this.baseUrl = "https://graph.instagram.com/";
   }
 
-  render (){
+  render() {
     return (
-     <Router>
-       <div>
-       <Route exact path ="/" render={({history}, props) =><Login history ={history} {...props}/>}/>
-       <Route exact path ="/login" render={({history}, props) =><Login history ={history} {...props}/>}/>
-         <Route exact path="/home" render={({history},props) =><Home history ={history} {...props} baseUrl={this.baseUrl}/>}/>
-         <Route path='/profile' render={({history},props) => <Profile history={history} {...props} baseUrl={this.baseUrl} />} />
-       </div>
-       </Router>
+      <Router>
+        <div>
+          <Route exact path="/" render={({ history }, props) => <Login history={history} {...props} />} />
+          <Route exact path="/login" render={({ history }, props) => <Login history={history} {...props} />} />
+          <Route exact path="/home" render={({ history }, props) => <Home history={history} {...props} baseUrl={this.baseUrl} />} />
+          <Route path='/profile' render={({ history }, props) => <Profile history={history} {...props} baseUrl={this.baseUrl} />} />
+        </div>
+      </Router>
     )
   }
 }
